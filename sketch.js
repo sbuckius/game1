6a9dbb45e85410a79b9c14e7be840c0b1d88b1ba
Sarah Buckius <210062217+sbuckius@users.noreply.github.com>
2025-05-28 11:26:19 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -147,7 +147,7 @@ function draw() {
   fill(0);
   textSize(12);
   textAlign(LEFT);
-  text("Does AI support you by saving you time **at home**?", 50, 100); // Top
+  text("Does AI support you by saving you time **at home**?", 50, 50); // Top
 
   if (showPattern) {
     drawJacquardPattern();
@@ -158,7 +158,7 @@ function draw() {
   fill(0);
   textSize(12);
   textAlign(LEFT);
-  text("Does AI support you by saving you time **at home**?", 50, height + 10); // Bottom
+  text("Does AI support you by saving you time **at home**?", 50, 450); // Bottom
 }
 
 
